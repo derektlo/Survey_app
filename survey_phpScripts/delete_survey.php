f2750e045a882d4fe5d45e7aa56b9a04ed561e10
Derek <derek.t.lo@gmail.com>
2012-08-02 17:24:59 -0400

    updates

--- survey_phpScripts/delete_survey.php
+++ survey_phpScripts/delete_survey.php
@@ -13,6 +13,7 @@
  
 	// set restaurantID to get categories for selected restaurant
  	$surveyID   = isset($_GET['surveyID']) ? $_GET['surveyID']  : "";
+   $surveyAuthKey   = isset($_GET['surveyAuthKey']) ? $_GET['surveyAuthKey']  : "";
 
 	// Connect to the Database server   
     $link = mysql_connect($host, $uid, $pwd) or die("Could not connect");
@@ -20,13 +21,14 @@
    //select the Database
    mysql_select_db($db) or die("Could not select database");
    
-   // Create an array to hold our results
-   $arr = array();
-   
    //Execute the query to delete survey
      $rs = mysql_query("DELETE FROM Surveys WHERE Survey_id = '$surveyID'");
    
    if ($rs) {
+
+         $rs = mysql_query("DELETE FROM Survey_Results WHERE Survey_authKey = '$surveyAuthKey'");
+
+
       echo 'Successful';
    }
    else {
