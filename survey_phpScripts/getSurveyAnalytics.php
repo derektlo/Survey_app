b67c715590c93bc23699489dff1df34a29ef691a
Derek <derek.t.lo@gmail.com>
2012-08-20 20:19:46 -0400

    updates

--- survey_phpScripts/getSurveyAnalytics.php
+++ survey_phpScripts/getSurveyAnalytics.php
@@ -13,6 +13,7 @@
  
 	// set restaurantID to get categories for selected restaurant
  	$surveyAuthKey   = isset($_GET['surveyAuthKey']) ? $_GET['surveyAuthKey']  : "";
+   $surveyType   = isset($_GET['surveyType']) ? $_GET['surveyType']  : "";
 	
 	// Connect to the Database server   
     $link = mysql_connect($host, $uid, $pwd) or die("Could not connect");
@@ -23,6 +24,9 @@
    // Create an array to hold our results
    $arrayOfOptionsValues = array();
    
+   if (($surveyType == "Letters") || ($surveyType == "Numbers") || ($surveyType == "Custom")) {
+
+      // GATHER ALL RESULTS FROM THE SURVEY
    $result = mysql_query("SELECT Option_value FROM Survey_Results WHERE Survey_authKey = '$surveyAuthKey'");
 
    // Add the rows to the array 
@@ -36,11 +40,29 @@
     return round(($a*100) / $total, 1) . '%';
    }, $arrayOfOptionsValues);
 
-   $container = array();
+  $container = array();
+
+   if ($surveyType == 'Custom') {
+      
+      // GATHER ALL OPTIONS FROM THE SURVEY BECAUSE THEY ARE CUSTOM
+      $options = mysql_query("SELECT Option_name FROM Survey_Results WHERE Survey_authKey = '$surveyAuthKey'");
+      $arrayOfOptions = array();
+
+      while($holdObj = mysql_fetch_array($options)) {
+      $arrayOfOptions[] = $holdObj['Option_name'];
+      }
+          // GENERATE FINAL ARRAY TO BE SENT BACK TO APP
+         array_push($container,$arrayOfOptionsValues,$arrayOfPercents, $arrayOfOptions);
+   }
+   else {
+   
+   // GENERATE FINAL ARRAY TO BE SENT BACK TO APP
    array_push($container,$arrayOfOptionsValues,$arrayOfPercents);
+   }
    
    // return the json result.
    echo '{"Results":'.json_encode($container).'}';
+   }
 
    // close connection 
    mysql_close();
