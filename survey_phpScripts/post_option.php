9d2629004727279dc0b29446a4732e5518a70852
Derek <derek.t.lo@gmail.com>
2012-08-24 00:49:45 -0400

    updates

--- survey_phpScripts/post_option.php
+++ survey_phpScripts/post_option.php
@@ -14,24 +14,40 @@
 	// set restaurantID to get categories for selected restaurant
  	$surveyAuthKey   = isset($_GET['surveyAuthKey']) ? $_GET['surveyAuthKey']  : "";
    $optionName = isset($_GET['optionName']) ? $_GET['optionName']  : "";
-	
-   echo $surveyAuthKey;
-   echo $optionName;
+   $surveyType = isset($_GET['surveyType']) ? $_GET['surveyType']  : "";
 
 	// Connect to the Database server   
     $link = mysql_connect($host, $uid, $pwd) or die("Could not connect");
    
    //select the Database
    mysql_select_db($db) or die("Could not select database");
-   
-   // Create an array to hold our results
-   $arr = array();
-   
+
+   $query;
+
+   if ($surveyType != 'Specific') {
    //Execute the query
-     $rs = mysql_query("UPDATE Survey_Results SET Option_value = Option_value+1 
+     $query = mysql_query("UPDATE Survey_Results SET Option_value = Option_value+1 
       WHERE Survey_authKey = '$surveyAuthKey' AND Option_name = '$optionName'");
+   }
+   else {
+
+      $checkValue = mysql_query("SELECT Match_value FROM Survey_Specific_Response WHERE Match_value = '$optionName'");
+
+      if (mysql_num_rows($checkValue) > 0) {
+            $query = mysql_query("UPDATE Survey_Specific_Response SET Hits = Hits+1 
+                                 WHERE Survey_authKey = '$surveyAuthKey' AND Matching_value = '$optionName'");
+      }
+      else {
+
+         $varZero = 0;
+         $keyValue = -1;
+
+         $query = mysql_query("INSERT INTO Survey_Specific_Response (Survey_authKey, Matching_value, Key_value, Hits) 
+                              VALUES ('$surveyAuthKey','$optionName','$keyValue','$varZero')");
+      }
+   }
    
-   if ($rs) {
+   if ($query) {
       echo 'Successful';
    }
    else {
