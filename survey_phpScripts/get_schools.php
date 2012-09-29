ae8cfcaef992ae9f19c26a774ee6b81b183acfdc
Derek <derek.t.lo@gmail.com>
2012-09-29 16:46:59 -0400

    adding school features

---
+++ survey_phpScripts/get_schools.php
@@ -0,0 +1,38 @@
+<?php
+   
+   // Set Database information
+   
+   // Database host
+   $host = '50.112.249.251'; 
+   // Database name
+   $db = 'survey_app_db'; 
+   // MySQL username
+   $uid = 'external_user';
+   // MySQL password
+   $pwd = 'jTJW7xTn8MrjDyRd';
+ 
+	// set restaurantID to get categories for selected restaurant
+ 	$email   = isset($_GET['email']) ? $_GET['email']  : "";
+	
+	// Connect to the Database server   
+    $link = mysql_connect($host, $uid, $pwd) or die("Could not connect");
+   
+   //select the Database
+   mysql_select_db($db) or die("Could not select database");
+   
+   // Create an iVar to hold our results
+   $schoolsArray = array();
+   //Execute the query
+   $rs = mysql_query("SELECT School FROM Classes WHERE Email = '$email'");
+   
+   while($obj = mysql_fetch_object($rs)) {
+       $schoolsArray[] = $obj;
+   }
+   
+   // return the json result.
+   echo '{"Results":'.json_encode($schoolsArray).'}';
+
+
+   // close connection 
+   mysql_close();
+?>
