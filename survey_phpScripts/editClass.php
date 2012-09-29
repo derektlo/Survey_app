e7c2f2a947860e85cc7d22cfc99845dd7b953b4e
Derek <derek.t.lo@gmail.com>
2012-09-29 11:54:56 -0400

    adding editClass feature

---
+++ survey_phpScripts/editClass.php
@@ -0,0 +1,41 @@
+<?php
+   
+   // Set Database information
+   
+   // Database host
+   $host = '50.112.249.251'; 
+   // Database name
+   $db = 'survey_app_db'; 
+   // MySQL username
+   $uid = 'external_user';
+   // MySQL password
+   $pwd = 'jTJW7xTn8MrjDyRd';
+ 
+	// set restaurantID to get categories for selected restaurant
+ 	$email   = isset($_GET['email']) ? $_GET['email']  : "";
+   $className = isset($_GET['className']) ? $_GET['className']  : "";
+   $school = isset($_GET['school']) ? $_GET['school']  : "";
+   $classSize = isset($_GET['classSize']) ? $_GET['classSize']  : "";
+
+   // Connect to the Database server   
+    $link = mysql_connect($host, $uid, $pwd) or die("Could not connect");
+   
+   //select the Database
+   mysql_select_db($db) or die("Could not select database");
+
+   $query;
+
+      $query = mysql_query("UPDATE Classes SET Full_class_name = $className AND School = $school AND Class_size = $classSize WHERE Email = $email");
+   
+
+   if ($query) {
+      echo 'Successful';
+   }
+   else {
+      echo 'Error';
+   }
+
+
+   // close connection 
+   mysql_close();
+?>
